03a9a033507671981edabafa2b686273388b5acd
Oxide <mohammedhaider15mo@gmail.com>
2024-08-06 14:06:37 +0300

    Add more sql queries

--- db/queries.js
+++ db/queries.js
@@ -4,16 +4,52 @@ class Queries {
   constructor() {}
 
   async getAllGames() {
-    const { rows } = await pool.query("SELECT * FROM games");
+    const { rows } = await this.query("SELECT * FROM games");
     return rows;
   }
   async getAllStudios() {
-    const { rows } = await pool.query("SELECT * FROM studios");
+    const { rows } = await this.query("SELECT * FROM studios");
     return rows;
   }
   async getAllGenres() {
-    const { rows } = await pool.query("SELECT * FROM genres");
+    const { rows } = await this.query("SELECT * FROM genres");
     return rows;
+  }
+
+  async getGame(gameId) {
+    const { rows } = await this.query(
+      "SELECT * FROM genres WHERE game_id = ($1)",
+      [gameId]
+    );
+    return rows;
+  }
+  async getTopGames() {
+    const { rows } = await this.query(
+      "SELECT * FROM genres ORDER BY rating DESC LIMIT(10)"
+    );
+    return rows;
+  }
+  async getGamesOrderByRating() {
+    const { rows } = await this.query(
+      "SELECT * FROM genres ORDER BY rating DESC"
+    );
+    return rows;
+  }
+  async getGamesOrderByNewest() {
+    const { rows } = await this.query(
+      "SELECT * FROM genres ORDER BY release_date DESC"
+    );
+    return rows;
+  }
+  async getGamesOrderByOldest() {
+    const { rows } = await this.query(
+      "SELECT * FROM genres ORDER BY release_date"
+    );
+    return rows;
+  }
+
+  async query(text, params) {
+    return pool.query(text, params);
   }
 }
 
